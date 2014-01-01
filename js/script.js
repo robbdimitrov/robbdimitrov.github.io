6331551b3815d220cc72587f2ab6eecf44c98fb3
Robert Dimitrov <robert_dimitrov@me.com>
2014-01-01 21:33:10 +0200

    Edit biography, make menu bar stick on top and invert colors.

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/**
+ * script.js
+ *
+ * Robert Dimitrov
+ *
+ * Copyright (c) 2013 Robert Dimitrov
+ */
+
+$(window).scroll(function() {
+	var $header = $("header");
+	var scrolledClass = "scrolled";
+
+    if ($(window).scrollTop() >= 10) {
+    	if (!$header.hasClass(scrolledClass)) {
+    		$header.addClass(scrolledClass);
+    		$("#logo").addClass(scrolledClass);
+    		$("#menu_button").addClass(scrolledClass);
+
+	        $("#menu").children().each(function () {
+			    $(this).addClass(scrolledClass);
+			});
+    	};
+    } else {
+    	if ($header.hasClass(scrolledClass)) {
+    		$header.removeClass(scrolledClass);
+    		$("#logo").removeClass(scrolledClass);
+    		$("#menu_button").removeClass(scrolledClass);
+
+	        $("#menu").children().each(function () {
+			    $(this).removeClass(scrolledClass);
+			});
+    	};
+    }
+});
+
+$("#menu_button").click(function (event) {
+	event.preventDefault();
+});
