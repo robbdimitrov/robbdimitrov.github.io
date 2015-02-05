f622fa7489845b174d40be439646368f9db804ef
Robert Dimitrov <robert_dimitrov@me.com>
2015-02-05 22:59:55 +0200

    Change colors.

--- js/script.js
+++ js/script.js
@@ -6,36 +6,9 @@
  * Copyright (c) 2013 Robert Dimitrov
  */
 
-$(window).scroll(function() {
-	var $header = $("header");
-	var scrolledClass = "scrolled";
-
-    if ($(window).scrollTop() >= 15) {
-    	if (!$header.hasClass(scrolledClass)) {
-    		$header.addClass(scrolledClass);
-    		$("#logo").addClass(scrolledClass);
-    		$("#menu_button").addClass(scrolledClass);
-
-	        $("#menu").children().each(function () {
-			    $(this).children().addClass(scrolledClass);
-			});
-    	};
-    } else {
-    	if ($header.hasClass(scrolledClass)) {
-    		$header.removeClass(scrolledClass);
-    		$("#logo").removeClass(scrolledClass);
-    		$("#menu_button").removeClass(scrolledClass);
-
-	        $("#menu").children().each(function () {
-			    $(this).children().removeClass(scrolledClass);
-			});
-    	};
-    }
-});
-
 $("#menu_button").click(function (event) {
     var $dropdownMenu = $("#dropdown");
-    
+
     $dropdownMenu.slideToggle(200);
 
     event.stopPropagation();
