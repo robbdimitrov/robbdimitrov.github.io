bbd969495672c5e3763e56f536570d52d50f1266
Robert Dimitrov <robert_dimitrov@me.com>
2026-06-11 02:31:57 +0300

    build: setup Tailwind CLI and GH Pages action

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Outfit', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+      colors: {
+        zinc: {
+          850: '#1f1f22',
+          950: '#09090b',
+        }
+      }
+    }
+  },
+  plugins: [],
+}
